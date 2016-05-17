97f3aa7513cd855fc8c853aea299985db49dbeb6
Denise-Laure <deniselaurearifon@live.fr>
2016-05-17 10:09:21 +0200

    Update Scrabble

--- exo1.php
+++ exo1.php
@@ -10,9 +10,9 @@ $taille_tableau = 15;
 
 //initialisation du tableau 
 
-for ($posy=0 ; $posy<$taille_tableau; $posy++) {
-	for ($posx=0 ; $posx<$taille_tableau ; $posx++) {
-    	$tab[$posy][$posx] = '<p style="display:inline; color:#ECEBEE;">#</p>';
+for ($posy=0 ; $posy<$taille_tableau ; $posy++) {
+  for ($posx=0 ; $posx<$taille_tableau ; $posx++) {
+      $tab[$posy][$posx] = '#';
     }
 }
 
@@ -21,36 +21,38 @@ $mot = array();
 $mot[0]= array('mot' => 'avion','posx' => 3, 'posy' => 3, 'direction' => 0);
 $mot[1]= array('mot' => 'papillon','posx' => 2, 'posy' => 3, 'direction' => 1);
 $mot[2]= array('mot' => 'fee','posx' => 7, 'posy' => 8, 'direction' => 0);
+$mot[3]= array('mot' => 'pelle','posx' => 6, 'posy' => 10, 'direction' => 1);
 
 foreach ($mot as $mot[$j] ) {
   $word = $mot[$j]['mot'];
   $posx = $mot[$j]['posx'];
   $posy = $mot[$j]['posy'];
   $direction = $mot[$j]['direction'];
-  $taille_mot = strlen($word);  
+  $taille_mot = strlen($word); 
 
 // boucle qui permet d'ecrire horizontalement ou verticalement
 
-	if ($direction == 1) {
-        // gauche à droite 
-  		for ($i=$posx ; $i<$taille_mot+$posx ;$i++ ) {
-     		$tab[$posy][$i] = $word[$i-$posx];
-  		}
-	} else {
-		// haut en bas
-  		for ($i=$posy ; $i<$taille_mot+$posy ;$i++ ) {
-    		$tab[$i][$posx] = $word[$i-$posy];
-  		}
-	}
+    if ($direction == 1) {
+           // gauche à droite 
+          for ($i=$posx ; $i<$taille_mot+$posx ;$i++ ) {
+              $tab[$posy][$i] = $word[$i-$posx];
+          }
+    } else {
+        // haut en bas
+          for ($i=$posy ; $i<$taille_mot+$posy ;$i++ ) {
+              $tab[$i][$posx] = $word[$i-$posy];
+          }
+    }
 }
 
 // boucle qui permet d'afficher le tableau 
 
 for ($i=0 ; $i<$taille_tableau ; $i++) {
         foreach ($tab[$i] as $tableau) {
-        echo  $tableau ;
+        echo $tableau;
         }
-	echo '<br>';
+  echo '<br>';
 }
 
 ?>
+
